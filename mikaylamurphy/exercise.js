24cfcb71cee8f736d72e2174b43b5b872e06b5cc
Mikayla <mikayla@mit.edu>
2017-02-22 10:53:43 -0500

    add ex

--- mikaylamurphy/exercise.js
+++ mikaylamurphy/exercise.js
@@ -20,7 +20,7 @@ exercise.findBiggerSalaries = function(){
     //
     //     Example: salaries larger than 300K is 8
     // ---------------------------------------------------
-
+    return exercise.salaries.filter(exercise.largerSalary)
 };
 
 // filter function
@@ -32,5 +32,5 @@ exercise.largerSalary = function largerSalary(item){
     //     Return a boolean (true/false) if item
     //     is larger than exercise.salary
     // ---------------------------------------------------
-
+    return item[18] > exercise.salary
 };
